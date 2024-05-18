ca9b485fc40648bf4c075ce8b812ea144dde9b30
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-05-18 21:38:48 +0545

    mongoconnect

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+//first approach
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
+//   } catch (error) {
+//     console.log(error);
+//     throw err;
+//   }
+// })();
